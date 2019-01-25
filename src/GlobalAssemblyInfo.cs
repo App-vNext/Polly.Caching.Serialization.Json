2552fd2def6123ae74949ce19f8ac60c71b31b44
reisenberger <software@reisenberger.net>
2019-01-25 20:48:12 +0000

    Update copyright year

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 [assembly: AssemblyProduct("Polly.Caching.Serialization.Json")]
 [assembly: AssemblyCompany("App vNext")]
 [assembly: AssemblyDescription("Polly.Caching.Serialization.Json is a Newtonsoft.Json serialization plug-in for the Polly CachePolicy.  Polly is a library that allows developers to express resilience and transient fault handling policies such as Retry, Circuit Breaker, Timeout, Bulkhead Isolation, and Fallback in a fluent and thread-safe manner.")]
-[assembly: AssemblyCopyright("Copyright (c) 2018, App vNext")]
+[assembly: AssemblyCopyright("Copyright (c) 2019, App vNext")]
 [assembly: CLSCompliant(false)] // Because not all of Newtonsoft.Json, on which we depend, is CLSCompliant.
 
 #if DEBUG
